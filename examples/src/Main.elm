db7389c5ddc138503c4be680be291598d3ad93da
Thomas Waters <twaters117@gmail.com>
2020-10-20 07:36:21 -0400

    Example file

---
+++ examples/src/Main.elm
@@ -0,0 +1,35 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html)
+
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+-- Init
+
+type alias Model = ()
+
+init : Model
+init =
+    ()
+
+-- Update
+
+type Msg = None
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        None ->
+            model
+
+-- View
+
+view : Model -> Html msg
+view _ =
+    Html.div [] []
